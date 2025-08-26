c813c30d57407e4a1c1bb4834a6668dff5a1b0d5
Adrian Rodriguez <adrianrodriguez@MacBook-Air-de-Miguel.local>
2025-08-26 16:04:39 -0600

    bakcuup

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -4,10 +4,14 @@ import "./style/Menu.css";
 export default function Menu() {
   return (
     <div className="menu-container">
-      <h1 className="menu-title">Zorre.Inc</h1>
+      <h1 className="menu-title">Clinica Ortiz</h1>
       <nav className="menu-nav">
-        <Link className="menu-link" to="/game">▶️ Play</Link>
-        <Link className="menu-link" to="/personalizar">⚙️ Personalizar</Link>
+        <Link className="menu-link" to="/game">
+          ▶️ Play
+        </Link>
+        <Link className="menu-link" to="/personalizar">
+          ⚙️ Personalizar
+        </Link>
       </nav>
     </div>
   );
